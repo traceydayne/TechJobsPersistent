2cd53603cbb3f2fa5d5545478abb25616111a2cc
Tracey Chandler <tracey.culhane@gmail.com>
2020-07-28 11:07:25 -0500

    part 1 completed

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -12,13 +14,21 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Job> jobs = context.Jobs.ToList();
+            return View(jobs);
         }
 
-        public IActionResult Add()
+        public IActionResult Add(AddEmployerViewModel addEmployerViewModel)
         {
             return View();
         }
---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobsPersistent.Models
+{
+    public class AddEmployerViewModel
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
